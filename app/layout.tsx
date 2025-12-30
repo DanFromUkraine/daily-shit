ca1534a93d56b7e082bf78d33401a684309c5f38
DanFromUkraine <ovsannikovdana91@gmail.com>
2025-12-30 19:31:42 +0100

    Created basic outline for tiptap editor.
    Added basic styles for all components + enhanced ones for todo checkbox

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 import { ReactNode } from "react";
-import "./globals.css";
 import { Provider } from "jotai";
 import { Caveat } from "next/font/google";
+import "@/src/styles/main.css";
 
 const caveat = Caveat({
-    subsets: ["latin"],
-    weight: ["400", "700"],
+  subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export default function layout({ children }: { children: ReactNode }) {
-    return (
-        <Provider>
-            <html lang="en" className={caveat.className}>
-                <body className="p-20 max-sm:p-11 paperBg">{children}</body>
-            </html>
-        </Provider>
-    );
+  return (
+    <Provider>
+      <html lang="en" className={caveat.className}>
+        <body className="p-20 max-sm:p-11 paperBg">{children}</body>
+      </html>
+    </Provider>
+  );
 }
--- src/components/MainInput/index.tsx
+++ src/components/MainInput/index.tsx
@@ -3,33 +3,68 @@
 import { NOTES_DEPENDENCIES } from "@/src/constants";
 import { currModeSelectedAtom } from "@/src/jotai/currentMode";
 import { useAutosizeTextArea } from "@/src/utils/useAutosize";
+import { EditorContent, useEditor } from "@tiptap/react";
 import { useAtom, useAtomValue } from "jotai";
 import { ChangeEventHandler, useMemo, useRef } from "react";
+import { Document } from "@tiptap/extension-document";
+import { Text } from "@tiptap/extension-text";
+import { Paragraph } from "@tiptap/extension-paragraph";
+import { ListKit } from "@tiptap/extension-list";
+import { Bold } from "@tiptap/extension-bold";
+import { Underline } from "@tiptap/extension-underline";
+import { Heading } from "@tiptap/extension-heading";
+import { Strike } from "@tiptap/extension-strike";
+import { HardBreak } from "@tiptap/extension-hard-break";
+import { HorizontalRule } from "@tiptap/extension-horizontal-rule";
 
 export default function MainInput() {
-    const currentMode = useAtomValue(currModeSelectedAtom);
-    const stableTextAtom = useMemo(
-        () => NOTES_DEPENDENCIES[currentMode],
-        [currentMode],
-    );
-    const [inputText, setInputText] = useAtom(stableTextAtom);
-    const textAreaRef = useRef<HTMLTextAreaElement>(null);
-    const onInputChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
-        setInputText(e.target.value);
-    };
+  // const currentMode = useAtomValue(currModeSelectedAtom);
+  // const stableTextAtom = useMemo(
+  //   () => NOTES_DEPENDENCIES[currentMode],
+  //   [currentMode],
+  // );
+  // const [inputText, setInputText] = useAtom(stableTextAtom);
+  // const textAreaRef = useRef<HTMLTextAreaElement>(null);
+  // const onInputChange: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
+  //   setInputText(e.target.value);
+  // };
 
-    useAutosizeTextArea({ textAreaRef, _value: inputText });
+  // useAutosizeTextArea({ textAreaRef, _value: inputText });
 
-    return (
-        <textarea
-            title="main-input"
-            name="main-input"
-            value={inputText}
-            onChange={onInputChange}
-            data-testid="main-input"
-            className="w-full outline-none text-2xl resize-none mb-3 overflow-hidden"
-            placeholder="Your temporary story starts here..."
-            ref={textAreaRef}
-        ></textarea>
-    );
+  const editor = useEditor({
+    immediatelyRender: false,
+    extensions: [
+      Document,
+      Text,
+      Paragraph,
+      ListKit,
+      Bold,
+      Underline,
+      Heading,
+      Strike,
+      HardBreak,
+      HorizontalRule,
+    ],
+    editorProps: {
+      attributes: {
+        class: "focus-visible:outline-none!",
+      },
+    },
+  });
+
+  return <EditorContent editor={editor} />;
 }
+
+/*
+
+<textarea
+  title="main-input"
+  name="main-input"
+  value={inputText}
+  onChange={onInputChange}
+  data-testid="main-input"
+  className="w-full outline-none text-2xl resize-none mb-3 overflow-hidden"
+  placeholder="Your temporary story starts here..."
+  ref={textAreaRef}
+></textarea>
+*/
